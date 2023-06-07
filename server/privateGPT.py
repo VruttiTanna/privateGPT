3adc0af8197449cddba5ed72d28f79923b28c3ea
VruttiTanna <tannavrutti@gmail.com>
2023-06-07 23:25:30 +0530

    Update privateGPT.py

--- server/privateGPT.py
+++ server/privateGPT.py
@@ -9,6 +9,18 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.docstore.document import Document
 from dotenv import load_dotenv
+import os
+from dotenv import load_dotenv
+from chromadb.config import Settings
+
+load_dotenv()
+
+# Define the Chroma settings
+CHROMA_SETTINGS = Settings(
+        chroma_db_impl='duckdb+parquet',
+        persist_directory="db/",
+        anonymized_telemetry=False
+)
 from langchain.document_loaders import (
     CSVLoader,
     EverNoteLoader,
