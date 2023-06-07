a745dc28a61c1ef03fe6b84e03914f516143b00c
VruttiTanna <tannavrutti@gmail.com>
2023-06-08 01:47:15 +0530

    Update privateGPT.py

--- server/privateGPT.py
+++ server/privateGPT.py
@@ -144,7 +144,7 @@ def main():
         if query:
             documents = load_documents(uploaded_files) if uploaded_files else []
             for document in documents:
-                st.markdown(f"**Uploaded Document:** {document.file_path}")
+                st.markdown(f"**Uploaded Document:** {document}")
             question, answer, source_data = get_answer(query)
             st.markdown(f"**Question:** {question}")
             st.markdown(f"**Answer:** {answer}")
