3f6e10e5fd1c27ee6175cc8455eb376c7275ce17
VruttiTanna <tannavrutti@gmail.com>
2023-06-09 01:37:53 +0530

    Update privateGPT.py

--- server/privateGPT.py
+++ server/privateGPT.py
@@ -1,13 +1,21 @@
 import streamlit as st
-from langchain.chains import RetrievalQA
-from langchain.vectorstores import Chroma
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.llms import GPT4All
 import os
 import glob
 from typing import List
 from langchain.docstore.document import Document
+from langchain.document_loaders import (
+    CSVLoader,
+    EverNoteLoader,
+    PDFMinerLoader,
+    TextLoader,
+    UnstructuredEmailLoader,
+    UnstructuredEPubLoader,
+    UnstructuredHTMLLoader,
+    UnstructuredMarkdownLoader,
+    UnstructuredODTLoader,
+    UnstructuredPowerPointLoader,
+    UnstructuredWordDocumentLoader,
+)
 
 # Load environment variables
 persist_directory = os.environ.get('PERSIST_DIRECTORY')
@@ -19,11 +27,10 @@ model_n_ctx = int(os.environ.get('MODEL_N_CTX'))
 # Map file extensions to document loaders and their arguments
 LOADER_MAPPING = {
     ".csv": (CSVLoader, {}),
-    # ".docx": (Docx2txtLoader, {}),
     ".doc": (UnstructuredWordDocumentLoader, {}),
     ".docx": (UnstructuredWordDocumentLoader, {}),
     ".enex": (EverNoteLoader, {}),
-    ".eml": (MyElmLoader, {}),
+    ".eml": (UnstructuredEmailLoader, {}),
     ".epub": (UnstructuredEPubLoader, {}),
     ".html": (UnstructuredHTMLLoader, {}),
     ".md": (UnstructuredMarkdownLoader, {}),
@@ -35,7 +42,7 @@ LOADER_MAPPING = {
     # Add more mappings for other file extensions and loaders as needed
 }
 
-class TextLoader:
+class CustomTextLoader:
     @staticmethod
     def load(file_path):
         with open(file_path, "r", encoding="utf-8") as file:
@@ -46,8 +53,8 @@ def load_single_document(file_path: str) -> Document:
     ext = "." + file_path.rsplit(".", 1)[-1]
     if ext in LOADER_MAPPING:
         loader_class, loader_args = LOADER_MAPPING[ext]
-        loader = loader_class()
-        return loader.load(file_path)[0]
+        loader = loader_class(file_path, **loader_args)
+        return loader.load()[0]
     raise ValueError(f"Unsupported file extension '{ext}'")
 
 def load_documents(source_dir: str) -> List[Document]:
@@ -57,33 +64,11 @@ def load_documents(source_dir: str) -> List[Document]:
         all_files.extend(glob.glob(os.path.join(source_dir, f"**/*{ext}"), recursive=True))
     return [load_single_document(file_path) for file_path in all_files]
 
-def load_model():
-    global llm
-    callbacks = [StreamingStdOutCallbackHandler()]
-    llm = GPT4All(model=model_path, n_ctx=model_n_ctx, backend='gptj', callbacks=callbacks, verbose=False)
-
-def get_answer(query):
-    embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
-    db = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
-    retriever = db.as_retriever()
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
-    res = qa(query)
-    answer, docs = res['result'], res['source_documents']
-
-    source_data = []
-    for document in docs:
-        source_data.append({"name": document.metadata.get("source")})
-
-    return answer, source_data
-
-# Load the model
-load_model()
-
 # Streamlit App
 st.title("PrivateGPT - Question Answering System")
 
 # File Upload
-uploaded_file = st.file_uploader("Upload a document", type=["txt"])
+uploaded_file = st.file_uploader("Upload a document", type=list(LOADER_MAPPING.keys()))
 
 if uploaded_file is not None:
     file_contents = uploaded_file.read().decode("utf-8")
@@ -106,10 +91,13 @@ if uploaded_file is not None:
     if st.button("Get Answer"):
         if question.strip() != "":
             # Get the answer using the loaded model
-            answer, source_data = get_answer(question)
+            answer, source_data = get_answer(question, db)
             st.write("Answer:", answer)
             st.write("Source Documents:")
             for source in source_data:
                 st.write("- ", source.get("name"))
         else:
             st.warning("Please enter a question.")
+else:
+    st.warning("Please upload a document.")
+
